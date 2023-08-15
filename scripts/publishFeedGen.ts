c1b94b91c90d9069312df3b03f9f43d60fc6b374
Ryan <morris.ryan.david@gmail.com>
2023-08-15 16:11:05 -0400

    changed the display name

--- scripts/publishFeedGen.ts
+++ scripts/publishFeedGen.ts
@@ -21,7 +21,7 @@ const run = async () => {
 
   // A display name for your feed
   // Ex: What's Hot
-  const displayName = 'Jewsky+'
+  const displayName = 'Jewsky Plus'
 
   // (Optional) A description of your feed
   // Ex: Top trending content from the whole network

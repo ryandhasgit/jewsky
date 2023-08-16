52536064176d0e06318527a8d5954c54e751ef2c
Ryan <morris.ryan.david@gmail.com>
2023-08-15 21:22:35 -0400

    commenting out some shit to see if this works

--- src/methods/feed-generation.ts
+++ src/methods/feed-generation.ts
@@ -9,16 +9,16 @@ export default function (server: Server, ctx: AppContext) {
   server.app.bsky.feed.getFeedSkeleton(async ({ params, req }) => {
     const feedUri = new AtUri(params.feed)
     const algo = algos[feedUri.rkey]
-    if (
-      feedUri.hostname !== ctx.cfg.publisherDid ||
-      feedUri.collection !== 'app.bsky.feed.generator' ||
-      !algo
-    ) {
-      throw new InvalidRequestError(
-        'Unsupported algorithm',
-        'UnsupportedAlgorithm',
-      )
-    }
+    // if (
+    //   feedUri.hostname !== ctx.cfg.publisherDid ||
+    //   feedUri.collection !== 'app.bsky.feed.generator' ||
+    //   !algo
+    // ) {
+    //   throw new InvalidRequestError(
+    //     'Unsupported algorithm',
+    //     'UnsupportedAlgorithm',
+    //   )
+    // }
     /**
      * Example of how to check auth if giving user-specific results:
      *
